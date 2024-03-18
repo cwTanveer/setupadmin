06adc539eea996900e88c87b94a4da2f9ce21909
Anish khan <anishkhanpro2@gmail.com>
2024-03-18 11:39:46 +0530

    register updated

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -1,68 +1,70 @@
 @extends('admin.layout.auth')
-@section('title','Login')
+@section('title','Register')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                            </div>
-                            @if(Session::has('fail'))
+<div class="container">
+
+<div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+            <div class="col-lg-7">
+                <div class="p-5">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                    </div>
+                    @if(Session::has('fail'))
                                 <small style="color:red">{{ Session::get('fail') }}</small>
                             @endif
-                            <form method="post" class="user">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="email" name="email" class="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                        placeholder="Enter Email Address...">
-                                        @error('email')
+                    <form method="post" class="user">
+                    @csrf
+                        <div class="form-group">
+                                <input type="text" name="name" class="form-control form-control-user" id="exampleFirstName"
+                                    placeholder="Name">
+                                    @error('name')
                                             <small style="color:red">{{$message}}</small>
                                         @enderror
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" name="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password">
-                                        @error('password')
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail"
+                                placeholder="Email Address">
+                                @error('email')
                                             <small style="color:red">{{$message}}</small>
                                         @enderror
-                                </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" name="remember" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Remember
-                                            Me</label>
+                        </div>
+                        <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name = "password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" name="password_confirmation" class="form-control form-control-user"
+                                            id="exampleRepeatPassword" placeholder="Repeat Password">
                                     </div>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Login
-                                </button>
-                                <hr>
-                                <a href="#" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="#" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="#">Forgot Password?</a>
-                            </div>
-                            <div class="text-center">
-                                <a class="small" href="#">Create an Account!</a>
-                            </div>
-                        </div>
+                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                        Register Account
+                        </button>
+                        <hr>
+                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                            <i class="fab fa-google fa-fw"></i> Register with Google
+                        </a>
+                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                            <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                        </a>
+                    </form>
+                    <hr>
+                    <div class="text-center">
+                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    </div>
+                    <div class="text-center">
+                        <a class="small" href="login.html">Already have an account? Login!</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/', function () {
 Route::get('dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::post('login',[AuthController::class,'loginPost']);
-
-
+Route::get('register',[AuthController::class,'register'])->name('register');
+Route::post('register',[AuthController::class,'registerPost']);
 
