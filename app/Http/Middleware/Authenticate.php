b062c94272cd52724ab7b0355eaface743e27239
Anish khan <anishkhanpro2@gmail.com>
2024-03-28 16:33:43 +0530

    update middleware functionality on dashboard

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate 
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    public function handle(Request $request, Closure $next)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (Auth::check()) {
+            return $next($request);
+        }
+        return redirect()->route('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
- 
-Route::get('dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
+   Route::group(['middleware' => 'auth'], function () {
+     Route::get('dashboard',[DashboardController::class,'index'])->name('admin.dashboard');  
+   });
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::post('login',[AuthController::class,'loginPost']);
 Route::get('register',[AuthController::class,'register'])->name('register');
